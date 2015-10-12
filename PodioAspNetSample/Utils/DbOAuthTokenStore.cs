6a3a6c904bb47a9ef26fa6e31a80564fb26a2b4c
Ajmal <avh@phases.dk>
2015-10-12 12:24:24 +0530

    refactor

--- PodioAspNetSample/Utils/DbOAuthTokenStore.cs
+++ PodioAspNetSample/Utils/DbOAuthTokenStore.cs
@@ -1,10 +1,5 @@
 ﻿using PodioAPI.Utils.Authentication;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Newtonsoft.Json;
-using PodioAspNetSample.Utils;
 using PodioAspNetSample.Models;
 
 namespace PodioAspNetSample
